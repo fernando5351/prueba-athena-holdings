f69440f27b46fc7f2e8e9407bbeae4fe446a40cb
fernando <isaacfernando5351@gmail.com>
2024-04-19 14:20:25 -0600

    :lipstick: items style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -81,7 +81,7 @@ export default function Home() {
         </div>
       </div>
       <div className="row justify-content-center">
-        <div className="col-12 col-lg-12 d-flex flex-wrap"> 
+        <div className="col-12 col-lg-12 d-flex flex-wrap justify-content-center">
           {filteredItems.map((item) => (
             <div className="col-12 col-md-6 col-lg-5 m-2" key={item.id}>
               <Task item={item} />
